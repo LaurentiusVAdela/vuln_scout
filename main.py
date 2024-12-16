4f4e8487f5f10af09f9b2b9b3443075826e643b5
Laurentius Valdi Adela <valdilaurentius@gmail.com>
2024-12-15 21:04:15 -0800

    Implement logging configurations in utils.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+from vuln_scout.parser import parse_requirements
+
 def main():
     parser = argparse.ArgumentParser(description="VulnScout: Dependency Vulnerability Scanner")
     parser.add_argument("--input", '-i', required=True, help="Path to the requirements.txt file")
@@ -9,6 +11,9 @@ def main():
 
     args = parser.parse_args()
 
+    # Parse dependencies
+    deps = parse_requirements(args.input)
+    print("Parsed dependencies:", deps)
     print(f"Input file: {args.input}")
     print(f"Output file: {args.output}")
     print(f"Report format: {args.format}")
